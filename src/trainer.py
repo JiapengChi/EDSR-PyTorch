2c554c26251219f0e586eb97853682c595a4ca3f
Jiapeng Chi <66231980+JiapengChi@users.noreply.github.com>
2020-12-03 07:32:46 -0500

    Nine Partition

--- src/trainer.py
+++ src/trainer.py
@@ -96,6 +96,11 @@ class Trainer():
         self.optimizer.schedule()
 
     def test(self):
+        d_gap = 128
+        width = 4096 / d_gap
+        height = 2048 / d_gap
+        list_visited = []
+
         torch.set_grad_enabled(False)
 
         epoch = self.optimizer.get_last_epoch()
@@ -117,6 +122,7 @@ class Trainer():
         # if self.args.save_results: self.ckp.begin_background()
         for idx_data, d in enumerate(self.loader_test):
             for idx_scale, scale in enumerate(self.scale):
+                print(self.loader_test)
                 d.dataset.set_scale(idx_scale)
                 for lr_ori, hr_ori, filename_ori in d:
                     data_dic_lr[filename_ori[0]] = lr_ori
@@ -140,63 +146,147 @@ class Trainer():
                     mmap_partition_num_3 = int(partition_mmap_file.read_byte())
                     # if mmap_partition_num_3 != 0:
                     #     print(mmap_partition_num_3)
-                    mmap_partition_num = str(1000 * mmap_partition_num_0 + 100 * mmap_partition_num_1 + 10 * mmap_partition_num_2 + 1 * mmap_partition_num_3 - 1)
+                    mmap_partition_num = str(
+                        1000 * mmap_partition_num_0 + 100 * mmap_partition_num_1 + 10 * mmap_partition_num_2 + 1 * mmap_partition_num_3 - 1)
                     if mmap_partition_num != loop_count and mmap_partition_num != '-1':
-                        loop_count = mmap_partition_num
+                        list_cand = []
+                        num_center = int(mmap_partition_num)
+                        if num_center not in list_visited:
+                            list_cand.append(num_center)
+                            list_visited.append(num_center)
 
-                        filename = 'Tile-' + mmap_partition_num
+                        num_top = num_center - width
+                        if num_top >= 0 and num_top not in list_visited:
+                            list_cand.append(num_top)
+                            list_visited.append(num_top)
 
-                        lr = data_dic_lr[filename]
-                        hr = data_dic_hr[filename]
-                        lr, hr = self.prepare(lr, hr)
-                        sr = self.model(lr, idx_scale)
-                        sr = utility.quantize(sr, self.args.rgb_range)
+                        num_bottom = num_center + width
+                        if num_bottom < width * height and num_bottom not in list_visited:
+                            list_cand.append(num_bottom)
+                            list_visited.append(num_bottom)
 
-                        save_list = [sr]
-                        self.ckp.log[-1, idx_data, idx_scale] += utility.calc_psnr(
-                            sr, hr, scale, self.args.rgb_range, dataset=d
-                        )
-                        if self.args.save_gt:
-                            save_list.extend([lr, hr])
-
-                        if self.args.save_results:
-                            self.ckp.save_results(d, filename[0], save_list, scale)
-
+                        num_left = num_center - 1
+                        if num_left <= int(num_center / width) * width and num_left not in list_visited:
+                            list_cand.append(num_left)
+                            list_visited.append(num_left)
+                        else:
+                            new_partition_num = (int(num_center / width) + 1) * width - 1
+                            if new_partition_num not in list_visited:
+                                list_cand.append(new_partition_num)
+                                list_visited.append(new_partition_num)
 
 
-                        postfix = ('SR', 'LR', 'HR')
-                        for v, p in zip(save_list, postfix):
-                            normalized = v[0].mul(255 / self.args.rgb_range)
-                            tensor_cpu = normalized.byte().permute(1, 2, 0).cpu()
+                        num_right = num_center + 1
+                        if num_right < (int(num_center / width) + 1) * width and num_right not in list_visited:
+                            list_cand.append(num_right)
+                            list_visited.append(num_right)
+                        else:
+                            new_partition_num = int(num_center / width) * width
+                            if new_partition_num not in list_visited:
+                                list_cand.append(new_partition_num)
+                                list_visited.append(new_partition_num)
 
-                            data_list = tensor_cpu.numpy().tolist()
-                            new_list = []
-                            get_list_num(data_list, new_list)
+                        num_top_left = num_center - width - 1
+                        if num_top_left >= 0:
+                            if num_top_left <= int(num_top / width) * width and num_top_left not in list_visited:
+                                list_cand.append(num_top_left)
+                                list_visited.append(num_top_left)
+                            else:
+                                new_partition_num = (int(num_top / width) + 1) * width - 1
+                                if new_partition_num not in list_visited:
+                                    list_cand.append(new_partition_num)
+                                    list_visited.append(new_partition_num)
 
-                            mmap_file.write(bytes(new_list))
+                        num_top_right = num_center - width + 1
+                        if num_top_right >= 0:
+                            if num_top_right < (int(num_top / width) + 1) * width and num_top_right not in list_visited:
+                                list_cand.append(num_top_right)
+                                list_visited.append(num_top_right)
+                            else:
+                                new_partition_num = int(num_top / width) * width
+                                if new_partition_num not in list_visited:
+                                    list_cand.append(new_partition_num)
+                                    list_visited.append(new_partition_num)
 
-                            # sr_list = []
-                            # sr_list.append(int(mmap_partition_num) + 1)
-                            # sr_mmap_file.write(bytes(sr_list))
+                        num_bottom_left = num_center + width - 1
+                        if num_bottom_left < width * height:
+                            if num_bottom_left <= int(num_bottom / width) * width and num_bottom_left not in list_visited:
+                                list_cand.append(num_bottom_left)
+                                list_visited.append(num_bottom_left)
+                            else:
+                                new_partition_num = (int(num_bottom / width) + 1) * width - 1
+                                if new_partition_num not in list_visited:
+                                    list_cand.append(new_partition_num)
+                                    list_visited.append(new_partition_num)
 
-                            sr_list = list(map(int, list(str(int(mmap_partition_num) + 1).zfill(4))))
-                            sr_mmap_file.write(bytes(sr_list))
+                        num_bottom_right = num_center + width + 1
+                        if num_bottom_right < width * height:
+                            if num_bottom_right >= 0:
+                                if num_bottom_right < (int(num_bottom / width) + 1) * width and num_bottom_right not in list_visited:
+                                    list_cand.append(num_bottom_right)
+                                    list_visited.append(num_bottom_right)
+                                else:
+                                    new_partition_num = int(num_bottom / width) * width
+                                    if new_partition_num not in list_visited:
+                                        list_cand.append(new_partition_num)
+                                        list_visited.append(new_partition_num)
 
-                            print("Partition " + mmap_partition_num)
+                        for cand in list_cand:
+                            mmap_partition_num = str(int(cand))
+                            loop_count = mmap_partition_num
 
-                        self.ckp.log[-1, idx_data, idx_scale] /= len(d)
-                        best = self.ckp.log.max(0)
-                        self.ckp.write_log(
-                            '[{} x{}]\tPSNR: {:.3f} (Best: {:.3f} @epoch {})'.format(
-                                d.dataset.name,
-                                scale,
-                                self.ckp.log[-1, idx_data, idx_scale],
-                                best[0][idx_data, idx_scale],
-                                best[1][idx_data, idx_scale] + 1
+                            filename = 'Tile-' + mmap_partition_num
+
+                            lr = data_dic_lr[filename]
+                            hr = data_dic_hr[filename]
+                            lr, hr = self.prepare(lr, hr)
+                            sr = self.model(lr, idx_scale)
+                            sr = utility.quantize(sr, self.args.rgb_range)
+
+                            save_list = [sr]
+                            self.ckp.log[-1, idx_data, idx_scale] += utility.calc_psnr(
+                                sr, hr, scale, self.args.rgb_range, dataset=d
                             )
-                        )
+                            if self.args.save_gt:
+                                save_list.extend([lr, hr])
 
+                            if self.args.save_results:
+                                self.ckp.save_results(d, filename[0], save_list, scale)
 
+                            postfix = ('SR', 'LR', 'HR')
+                            for v, p in zip(save_list, postfix):
+                                normalized = v[0].mul(255 / self.args.rgb_range)
+                                tensor_cpu = normalized.byte().permute(1, 2, 0).cpu()
+
+                                data_list = tensor_cpu.numpy().tolist()
+                                new_list = []
+                                get_list_num(data_list, new_list)
+
+                                partitionBytesPosition = d_gap * d_gap * 3 * int(mmap_partition_num)
+                                mmap_file.seek(partitionBytesPosition)
+
+                                mmap_file.write(bytes(new_list))
+
+                                # sr_list = []
+                                # sr_list.append(int(mmap_partition_num) + 1)
+                                # sr_mmap_file.write(bytes(sr_list))
+
+                                sr_list = list(map(int, list(str(int(mmap_partition_num) + 1).zfill(4))))
+                                sr_mmap_file.write(bytes(sr_list))
+
+                                print("Partition " + mmap_partition_num)
+
+                            self.ckp.log[-1, idx_data, idx_scale] /= len(d)
+                            best = self.ckp.log.max(0)
+                            self.ckp.write_log(
+                                '[{} x{}]\tPSNR: {:.3f} (Best: {:.3f} @epoch {})'.format(
+                                    d.dataset.name,
+                                    scale,
+                                    self.ckp.log[-1, idx_data, idx_scale],
+                                    best[0][idx_data, idx_scale],
+                                    best[1][idx_data, idx_scale] + 1
+                                )
+                            )
 
         time.sleep(60)
         mmap_file.close()
